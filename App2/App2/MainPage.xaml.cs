85d31ab9ca0e8db1e6d5bffda22aeda17cd6e597
ToxCilium <sereda_maksimka@bk.ru>
2021-09-28 14:30:18 +0300

    First commit

---
+++ App2/App2/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void Button_Clicked(object sender, System.EventArgs e)
+        {
+            string xaml = "<Label Text=\"Xamarin Forms\" FontSize=\"24\" TextColor=\"Red\" />";
+            lbl.LoadFromXaml(xaml);
+        }
+
+    }
+}
